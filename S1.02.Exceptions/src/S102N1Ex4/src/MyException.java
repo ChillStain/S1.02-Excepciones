1093d9637f996dc2b05456386e127abb03d6ccd4
ChillStain <101631488+ChillStain@users.noreply.github.com>
2022-04-27 09:38:17 +0200

    Add files via upload

---
+++ S1.02.Exceptions/src/S102N1Ex4/src/MyException.java
@@ -0,0 +1,13 @@
+public class MyException extends Exception {
+
+    String message;
+
+    public MyException(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+}
